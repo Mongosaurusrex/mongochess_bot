0486ad537b35da0a3d8afa56ab61153eef1b99d2
Nathan Dygant <nathan.dygant@gmail.com>
2023-12-22 13:21:19 +0100

    Origin typeo

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ class ChessState(BaseModel):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["comforting-croissant-0cc459.netlify.app"],
+    allow_origins=["https://comforting-croissant-0cc459.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
